baa5a1b55f040a85040d625f744aeb5dc4d2b6d4
Hieu Nguyen <hieuk09@gmail.com>
2019-11-04 10:36:19 +0800

    Add type to account_decorator & basic_decorator

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -1,20 +1,25 @@
-# typed: true
+# typed: strict
 class AccountDecorator
+  extend T::Sig
   delegate :name, :id, to: :account
 
+  sig {params(account: Account).void}
   def initialize(account)
-    @account = account
+    @account = T.let(account, Account)
   end
 
+  sig {returns(String)}
   def initial_balance
     account.initial_balance.format
   end
 
+  sig {returns(T::Array[TransactionsByDateDecorator::ByDateDecorator])}
   def transactions
     TransactionsByDateDecorator.new(account.transactions.order(:paid_at)).decorate
   end
 
   private
 
+  sig {returns(Account)}
   attr_reader :account
 end
--- app/decorators/categories/basic_decorator.rb
+++ app/decorators/categories/basic_decorator.rb
@@ -1,22 +1,28 @@
-# typed: true
+# typed: strict
 module Categories
   class BasicDecorator
+    extend T::Sig
     include Comparable
 
     delegate :name, :category_type, :id, :name, to: :category
 
+    sig {params(category: Category).void}
     def initialize(category)
-      @category = category
+      @category = T.let(category, Category)
+      @total = T.let(nil, T.nilable(Money))
     end
 
+    sig {returns(Float)}
     def total_usd
       total.exchange_to(:usd).to_f
     end
 
+    sig {returns(Money)}
     def total
       @total ||= category.total
     end
 
+    sig {params(other: Categories::BasicDecorator).returns(Integer)}
     def <=>(other)
       if category_type == other.category_type
         total <=> other.total
@@ -25,16 +31,19 @@ module Categories
       end
     end
 
+    sig {returns(T::Boolean)}
     def income?
       category_type == Category::INCOME
     end
 
+    sig {returns(T::Boolean)}
     def expense?
       category_type == Category::EXPENSE
     end
 
     private
 
+    sig {returns(Category)}
     attr_reader :category
   end
 end
