7982631db29ef3d5a9c86ef374d3a6ec2bfea4c8
Hieu Nguyen <hieuk09@gmail.com>
2020-08-17 14:46:36 +0800

    Add tests for PdfExtractor

---
+++ spec/services/pdf_extractor_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe PdfExtractor do
+  describe '#extract' do
+    let(:text) do
+      <<-TEXT
+       Account
+
+       21 Jul           Debit Card Transaction                                      14.80
+                        THE GREEN PARTY    SI NG
+                        xxxx-xxxx-xxxx-xxxx
+
+       21 Jul           Debit Card Transaction                                                    60.00
+                        TOYS'R'US (S) PL - SUN SI NG 19JUL
+                        xxxx-xxxx-xxxx-xxxx
+
+      TEXT
+    end
+    let(:pages) { [double(text: text)] }
+    let(:reader) { instance_double(PDF::Reader, pages: pages) }
+    let(:io) { double }
+    let(:extractor) { described_class.new }
+    let(:expected_transactions) do
+      [
+        {
+          deposit: nil,
+          description: 'THE GREN PARTY SI NG x-x-x-x',
+          paid_at: DateTime.new(2020, 7, 21),
+          withdrawal: BigDecimal('14.80')
+        },
+        {
+          deposit: BigDecimal('60'),
+          description: "TOYS'R'US (S) PL - SUN SI NG 19JUL x-x-x-x",
+          paid_at: DateTime.new(2020, 7, 19),
+          withdrawal: nil
+        }
+      ]
+    end
+    subject { extractor.extract(io) }
+
+    before do
+      expect(PDF::Reader).to receive(:new)
+        .with(io).and_return(reader)
+    end
+
+    it { is_expected.to eq expected_transactions }
+  end
+end
